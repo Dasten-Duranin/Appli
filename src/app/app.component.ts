836230364790f6c314edf63c932e2ed80dc7152d
Tanguy Le Roux <leroux.tanguy.51@gmail.com>
2017-10-05 09:52:35 +0200

    Récupération des données

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+	title = 'app';
+
+	weather   = null;
+	pollition = null;
+	warning   = null;
+	theft     = null;
+
+	constructor(private http: Http) {
+
+	}
+
+	getWeather(): any {
+		return this.http.get('http://192.168.43.89:8000/weather/')
+			.toPromise()
+			.then(response => response.json())
+			.catch(this.handleError);
+	}
+
+	getPollution(): any {
+		return this.http.get('http://192.168.43.89:8001/pollution/')
+			.toPromise()
+			.then(response => response.json())
+			.catch(this.handleError);
+	}
+
+	getWarning(): any {
+		return this.http.get('http://192.168.43.89:8002/alert/')
+			.toPromise()
+			.then(response => response.json())
+			.catch(this.handleError);
+	}
+
+	getTheft(): any {
+		return this.http.get('http://192.168.43.89:8004/vol')
+			.toPromise()
+			.then(response => response.json())
+			.catch(this.handleError);
+	}
+
+	private handleError(error: any): Promise<any> {
+		return Promise.reject(error);
+	}
+
+	ngOnInit(): void {
+		this.getWeather().then(rep => {this.weather = rep; console.log(this.weather)});
+		this.getPollution().then(rep => {this.pollition = rep; console.log(this.pollition)});
+		this.getWarning().then(rep => {this.warning = rep; console.log(this.warning)});
+		this.getTheft().then(rep => {this.theft = rep; console.log(this.theft)});
+	}
 }
